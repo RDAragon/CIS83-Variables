b7f37a87cd19c2bc1195af8274f4be7edf67dc8f
RDAragon <42855144+RDAragon@users.noreply.github.com>
2019-02-25 02:46:39 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+
+num1 = int(input('Enter first number: \n'))
+num2 = int(input('Enter second  number: \n'))
+
+print('The sum is: ',num1 + num2)
+print('The diffrence is: ',num1 - num2)
+print('The product is: ',num1 * num2)
+print('The average is: ',(num1 + num2) /2)
+print('The distance is: ',math.fabs(num1 - num2))
+print('The maximum is: ',max(num1,num2))
+print('The minimum is: ',min(num1,num2))
+
